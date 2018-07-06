db220762bf713fff8475b0285e9a9c63f25ef2e7
Erin Highbaugh <33356214+ehighbaugh@users.noreply.github.com>
2018-07-06 17:03:33 -0400

    Created enrollment group viewmodel on about page

--- GranadaU/Models/Student.cs
+++ GranadaU/Models/Student.cs
@@ -2,14 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace GranadaU.Models
 {
     public class Student
     {
         public int ID { get; set; }
+        [StringLength(50)]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]
         public string LastName { get; set; }
+        [StringLength(50, ErrorMessage = "First name cannot be longer than 50 characters")]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]
         public string FirstMidName { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString ="{0:yyyy-MM-dd", ApplyFormatInEditMode =true)]
         public DateTime EnrollmentDate { get; set; }
 
         public virtual ICollection<Enrollment> Enrollments { get; set; }
