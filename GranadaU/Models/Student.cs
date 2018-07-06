77e7a228a98bc4af500442bad198acb0161d873e
Erin Highbaugh <33356214+ehighbaugh@users.noreply.github.com>
2018-07-06 17:53:36 -0400

    created Instructor and OFficeAssignment models

--- GranadaU/Models/Student.cs
+++ GranadaU/Models/Student.cs
@@ -1,23 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace GranadaU.Models
 {
     public class Student
     {
         public int ID { get; set; }
+
+        [Required]
+        [Display(Name ="Last Name")]
         [StringLength(50)]
         [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]
         public string LastName { get; set; }
+
+        [Required]
+        [Display(Name = "First Name")]
         [StringLength(50, ErrorMessage = "First name cannot be longer than 50 characters")]
         [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]
+        [Column("FirstName")]
         public string FirstMidName { get; set; }
-        //[DataType(DataType.Date)]
-        //[DisplayFormat(DataFormatString ="{0:yyyy-MM-dd", ApplyFormatInEditMode =true)]
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Enrollment Date")]
         public DateTime EnrollmentDate { get; set; }
+
+        [Display(Name = "Full Name")]
+        public string FullName
+        {
+            get
+            {
+                return LastName + ", " + FirstMidName;
+            }
+        }
 
         public virtual ICollection<Enrollment> Enrollments { get; set; }
     }
