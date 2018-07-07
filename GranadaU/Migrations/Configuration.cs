3386759c9cca73f2380d246c2b4bce8c0403048e
Erin Highbaugh <33356214+ehighbaugh@users.noreply.github.com>
2018-07-07 00:32:41 -0400

    initialized migrations

---
+++ GranadaU/Migrations/Configuration.cs
@@ -0,0 +1,24 @@
+namespace GranadaU.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<GranadaU.Data.SchoolContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "GranadaU.Data.SchoolContext";
+        }
+
+        protected override void Seed(GranadaU.Data.SchoolContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
