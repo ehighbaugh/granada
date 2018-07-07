cf14b2c9d22711a234acc44006005dc11aa0f311
Erin Highbaugh <33356214+ehighbaugh@users.noreply.github.com>
2018-07-06 22:07:27 -0400

    created migrations

---
+++ GranadaU/Migrations/201807070150319_MigrationConfiguration.cs
@@ -0,0 +1,16 @@
+namespace GranadaU.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class MigrationConfiguration : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
