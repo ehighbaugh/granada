f1d67e6fc48990c91ed4d6788072d121866c55d5
Erin Highbaugh <33356214+ehighbaugh@users.noreply.github.com>
2018-07-08 10:50:32 -0400

    created person model; instructor/student inherit from person; added migration for inheritance; altered Up method to create person table and move over student/instructor data

--- GranadaU/Models/Instructor.cs
+++ GranadaU/Models/Instructor.cs
@@ -9,28 +9,12 @@ namespace GranadaU.Models
 {
     public class Instructor : Person
     {
-        public abstract class Person
-        {
-            public int ID { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Hire Date")]
+        public DateTime HireDate { get; set; }
 
-            [Required]
-            [StringLength(50)]
-            [Display(Name = "Last Name")]
-            public string LastName { get; set; }
-            [Required]
-            [StringLength(50, ErrorMessage = "First name cannot be longer than 50 characters.")]
-            [Column("FirstName")]
-            [Display(Name = "First Name")]
-            public string FirstMidName { get; set; }
-
-            [Display(Name = "Full Name")]
-            public string FullName
-            {
-                get
-                {
-                    return LastName + ", " + FirstMidName;
-                }
-            }
-        }
+        public virtual ICollection<Course> Courses { get; set; }
+        public virtual OfficeAssignment OfficeAssignment { get; set; }
     }
 }
